968646f2a7c746f7aa36395d1c4e3789c58bfddb
chystarr <chynnastarr2@gmail.com>
2023-03-11 07:04:54 -0500

    Added content for history page

--- src/components/History.js
+++ src/components/History.js
@@ -2,17 +2,21 @@ import React from "react";
 
 import "./css/History.css";
 import InfoCard from "./InfoCard";
-import placeholder from "./img/placeholder.jpg";
+import galileo from "./img/galileo.jpeg";
+import jupiterRings from "./img/jupiter-rings.jpg";
+import juno from "./img/juno.jpg";
 
 function History() {
-  const header = "Info component header example!!";
-  const text = "Some information about Jupiter. It's a planet in outer space. It's a gas planet. Some information about Jupiter. It's a planet in outer space. It's a gas planet. Some information about Jupiter. It's a planet in outer space. It's a gas planet. Some information about Jupiter. It's a planet in outer space. It's a gas planet.";
+  const headers = ["Galileo's Discovery", "Voyager 1", "Juno"];
+  const descriptions = ["Jupiter was first observed by the famous astronomer Galileo Galilei in 1610.",
+    "Saturn isn't the only planet with rings. In 1979, the Voyager 1 spacecraft found that Jupiter has rings as well.",
+    "In 2016, the Juno spacecraft arrived at Saturn in order to investigate things such as the composition of the planet's atmosphere."];
 
   return (
     <div className="history-page">
-      <InfoCard image={placeholder} header={header} text={text}/>
-      <InfoCard image={placeholder} header={header} text={text}/>
-      <InfoCard image={placeholder} header={header} text={text}/>
+      <InfoCard image={galileo} header={headers[0]} text={descriptions[0]}/>
+      <InfoCard image={jupiterRings} header={headers[1]} text={descriptions[1]}/>
+      <InfoCard image={juno} header={headers[2]} text={descriptions[2]}/>
     </div>
   );
 }
